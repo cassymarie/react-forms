5f6d9ed40bdf31edf61c89c99a5b59b08aff8eb5
cassymarie <cassy2205@gmail.com>
2021-02-25 16:36:18 -0700

    commit tree 98ccaa0d6831b2f7eea4c4feef30373c4e40f050

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,77 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
-  }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
+  // All below now handled in ParentComponent
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
+  // state = {
+  //   firstName: "John",
+  //   lastName: "Henry",
+  //   submittedData: []
+  // }
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+  //   let dataArray = this.state.submittedData.concat(formData)
+  //   this.setState({submittedData: dataArray})
+  // }
+
+  // handleFirstNameChange = (e) => {
+  //   this.setState({
+  //     firstName: e.target.value
+  //   })
+  // }
+
+  // handleLastNameChange = (e) => {
+  //   this.setState({
+  //     lastName: e.target.value
+  //   })
+  // }
+
+  // listOfSubmissions = () => {
+  //   return this.state.submittedData.map(data => {
+  //     return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+  //   })
+  // }
 
   render() {
+    // Step 1
+
+    // return (
+    //   <form>
+    //     <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+    //     <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+    //   </form>
+    // )
+    // Controlled Form
+
+    // Step 2 - Controlled Form (Sibling Element)
+    // return(
+    //   <div>
+    //     <form onSubmit={this.handleSubmit}>
+    //       <input type="text" name="firstName" onChange={this.handleFirstNameChange} value={this.state.firstName} />
+    //       <input type="text" name="lastName" onChange={this.handleLastNameChange} value={this.state.lastName} />
+    //       <input type="submit"/>
+    //     </form>
+    //     {this.listOfSubmissions()}
+    //   </div>
+    // )
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={event => this.props.handleFirstNameChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            onChange={event => this.props.handleLastNameChange(event)}
+            value={this.props.formData.lastName}
+          />
+        </form>
+      </div>
     )
   }
 }
